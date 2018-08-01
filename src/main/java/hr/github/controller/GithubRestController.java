4201dc679c15e0541de7134678c6be30d51960e8
Hirabayashi623 <ryo662233@gmail.com>
2018-08-02 00:59:03 +0900

    2018/08/02  0:59:03.07

--- src/main/java/hr/github/controller/GithubRestController.java
+++ src/main/java/hr/github/controller/GithubRestController.java
@@ -31,7 +31,7 @@ public class GithubRestController {
 
 			commits = CommitUtil.getCimmitIdList(body);
 
-			String url = json.getString("commits_url");
+			String url = json.getJSONObject("repository").getString("commits_url");
 			url = url.replaceAll("{.*}", "");
 			commitStr = url;
 
