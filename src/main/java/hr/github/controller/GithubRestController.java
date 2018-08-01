3ed3d189fdf5a61e4a534ec7b437915c46040b87
Hirabayashi623 <ryo662233@gmail.com>
2018-08-01 23:45:33 +0900

    2018/08/01 23:45:33.33

--- src/main/java/hr/github/controller/GithubRestController.java
+++ src/main/java/hr/github/controller/GithubRestController.java
@@ -1,9 +1,15 @@
 package hr.github.controller;
 
+import java.util.LinkedHashMap;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import hr.github.bean.GithubTmpBean;
 import hr.github.repository.GithubRepository;
@@ -15,13 +21,29 @@ public class GithubRestController {
 
 	@RequestMapping("/github/webhook/push")
 	public Object webhook(@RequestBody String body) {
+		// jsonデータをマップにバインド
+		ObjectMapper mapper = new ObjectMapper();
+		Map<String, Object> map = new LinkedHashMap<>();
+		try {
+			map = mapper.readValue(
+					body,
+					new TypeReference<LinkedHashMap<String, Object>>() {
+					});
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		String commitStr = "";
+		commitStr = map.get("commits").toString();
+
 		GithubTmpBean obj = new GithubTmpBean();
-		obj.setBody(body);
+		obj.setBody(commitStr);
 
 		repo.saveAndFlush(obj);
 
 		return new Object() {
 			private String result = "ok";
+
 			public String getResult() {
 				return this.result;
 			}
