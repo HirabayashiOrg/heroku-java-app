f67783380d58aedb3b8c9e73fb33c2a397e3433c
Hirabayashi623 <ryo662233@gmail.com>
2018-08-02 00:35:53 +0900

    2018/08/02  0:35:53.18

--- src/main/java/hr/github/controller/GithubRestController.java
+++ src/main/java/hr/github/controller/GithubRestController.java
@@ -1,5 +1,8 @@
 package hr.github.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -19,13 +22,14 @@ public class GithubRestController {
 	@RequestMapping("/github/webhook/push")
 	public Object webhook(@RequestBody String body) {
 		String commitStr = "";
+		List<String> commits = new ArrayList<String>();
 		// jsonデータをマップにバインド
 		try {
 			JSONObject json = new JSONObject(body);
 			JSONArray json_ary = json.getJSONArray("commits");
 			for(int i=0; i < json_ary.length(); i++) {
 				JSONObject json_obj = json_ary.getJSONObject(i);
-				commitStr += json_obj.getString("id") + ", ";
+				commits.add(json_obj.getString("id"));
 			}
 
 			commitStr += json.getJSONObject("comments_url");
