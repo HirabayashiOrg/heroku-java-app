86cdffa957ac4aafe9bf98090c423b6263e19b85
Hirabayashi623 <ryo662233@gmail.com>
2018-08-03 23:33:33 +0900

    2018/08/03 23:33:33.13

--- src/main/java/hr/github/controller/GithubRestController.java
+++ src/main/java/hr/github/controller/GithubRestController.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -63,5 +64,10 @@ public class GithubRestController {
 		repo.flush();
 
 		return;
+	}
+
+	@GetMapping("/github/api/commit")
+	public Object commit() {
+		return new Object();
 	}
 }
