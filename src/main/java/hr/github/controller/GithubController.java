eda0940e3d01f92bd364d87e25698c5406071b85
Hirabayashi623 <ryo662233@gmail.com>
2018-08-04 00:33:40 +0900

    2018/08/04  0:33:40.04

---
+++ src/main/java/hr/github/controller/GithubController.java
@@ -0,0 +1,27 @@
+package hr.github.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+import hr.github.bean.GithubPushBean;
+import hr.github.repository.GithubPushRepository;
+
+@RestController
+public class GithubController {
+	@Autowired
+	GithubPushRepository repo;
+
+	@GetMapping("/github/pushes")
+	public ModelAndView pushes(ModelAndView mav) {
+		List<GithubPushBean> pushes = repo.findAll();
+
+		mav.setViewName("github/push_history");
+		mav.addObject("pushes", pushes);
+
+		return mav;
+	}
+}
