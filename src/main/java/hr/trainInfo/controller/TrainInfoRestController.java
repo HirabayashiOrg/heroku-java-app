ad1c62526abcbf7a06cf7fffd4a94dabf7a6a981
Hirabayashi623 <ryo662233@gmail.com>
2018-08-05 01:41:17 +0900

    2018/08/05  1:41:17.57

--- src/main/java/hr/trainInfo/controller/TrainInfoRestController.java
+++ src/main/java/hr/trainInfo/controller/TrainInfoRestController.java
@@ -1,15 +1,19 @@
 package hr.trainInfo.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
+import hr.trainInfo.bean.TrainInfoNotificationBean;
 import hr.trainInfo.bean.TrainInfoRegisterBean;
 import hr.trainInfo.repository.TrainInfoNotificationRepository;
 import hr.trainInfo.service.TrainInfoRegisterService;
 
-@Controller
+@RestController
 public class TrainInfoRestController {
 	@Autowired
 	TrainInfoNotificationRepository repo;
@@ -22,5 +26,10 @@ public class TrainInfoRestController {
 		TrainInfoRegisterBean bean = registerService.register(line);
 
 		return bean;
+	}
+
+	@GetMapping("/trainInfo/info/notice/list")
+	public List<TrainInfoNotificationBean> info_notice_list() {
+		return repo.findAll();
 	}
 }
